721d3b7e1eb5be89fde70e9036016b181b61eb6d
Danny Phillips <danny.phillips8@gmail.com>
2018-02-01 15:05:52 -0800

    create dynamic mount

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("app-mount"));
+const mount = (node, props) => {
+  ReactDOM.render(<App {...props} />, node);
+}
+if (window.ConstructionOS) {
+  window.ConstructionOS.Applications.EquipmentTracker = {
+    mount: mount
+  };
+}
+else {
+  ReactDOM.render(<App />, document.getElementById("app-mount"));
+}
