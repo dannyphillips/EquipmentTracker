c362e0f0414118845986657057f445b2f5cc1c75
Danny Phillips <danny.phillips8@gmail.com>
2019-06-18 21:46:29 -0700

    Updates

---
+++ precache-manifest.3857ec1b23582555897ffdc6c95bfb29.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1924271dfbc365a9e8fa08cba96fbfa6",
+    "url": "https://dannyphillips.github.io/EquipmentTracker/index.html"
+  },
+  {
+    "revision": "0300e5607b01c1dbad96",
+    "url": "https://dannyphillips.github.io/EquipmentTracker/static/js/2.00ca21b4.chunk.js"
+  },
+  {
+    "revision": "4ca7ea7eef5cd71417d6",
+    "url": "https://dannyphillips.github.io/EquipmentTracker/static/js/main.b9837cc9.chunk.js"
+  },
+  {
+    "revision": "c15bfb203b106fd5c4ad",
+    "url": "https://dannyphillips.github.io/EquipmentTracker/static/js/runtime~main.41100dc4.js"
+  }
+]);
